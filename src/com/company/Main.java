2ac0e6055a9c516412eb100207db793292485c66
pio-mon <piotr0807@gmail.com>
2019-12-04 21:11:50 +0100

    zmiana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("witaj swiecie");
+        Math M = new Math();
 
 
-	// write your code here
+
     }
+
+
+    // write your code here
+}
 }
